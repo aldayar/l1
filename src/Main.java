1dfe09f8d61b1dc2df6fe76c18235af79a817939
aldayar <abdigaparaldayar@gmail.com>
2023-01-16 20:46:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner s=new Scanner(System.in);
+        System.out.println("Введите ваше имя");
+        int a= s.nextInt();
+        System.out.println("Здраствуйте "+s);
+
+
+
+
+
+    }
+}
